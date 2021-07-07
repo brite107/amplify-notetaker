acbc9cccf455eedefd59c353ed0fa077fb3ed467
jen-instantteams <jen.perry@instantteams.com>
2021-07-07 07:11:37 -0500

    add note form and list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState } from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      note: "Hello World"
+    }
+  ]);
+
   return (
-    <div>
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-1">Amplify Notetaker</h1>
+      {/* Note Form */}
+      <form className="mb3">
+        <input 
+          type="text" 
+          className="pa2 f4"
+          placeholder="Write your note" 
+        />
+        <button 
+          className="pa2 f4"
+          type="submit">
+          Add note
+        </button>
+      </form>
+      {/* Notes List */}
+      <div>
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">
+              {item.note}
+            </li>
+            <button className="bg-transparent bn f4"><span>&times;</span></button>
+          </div>
+        ))}
+      </div>
       <AmplifySignOut />
-      App
     </div>
   );
 }
