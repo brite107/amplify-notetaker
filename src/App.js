96341f66f45543504c55c441d4ff760619ce061f
jen-instantteams <jen.perry@instantteams.com>
2021-07-09 19:02:06 -0500

    add note

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import { useState } from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { API, graphqlOperation  } from 'aws-amplify';
+import { createNote } from './graphql/mutations'
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      note: "Hello World"
-    }
-  ]);
+  const [state, setState] = useState({
+    note: "",
+    notes: []
+  });
+
+  const handleChangeNote = event => {
+    console.log(event);
+    setState({note: event.target.value})
+  };
+
+  const handleAddNote = event => {
+    console.log(event);
+    const { note } = state;
+    event.preventDefault();
+    const input = { note };
+    API.graphql(graphqlOperation(createNote, { input }));
+  }
 
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
       <h1 className="code f2-1">Amplify Notetaker</h1>
       {/* Note Form */}
-      <form className="mb3">
+      <form onSubmit={handleAddNote} className="mb3">
         <input 
           type="text" 
           className="pa2 f4"
-          placeholder="Write your note" 
+          placeholder="Write your note"
+          onChange={handleChangeNote} 
         />
         <button 
           className="pa2 f4"
@@ -27,7 +41,7 @@ const App = () => {
       </form>
       {/* Notes List */}
       <div>
-        {notes.map(item => (
+        {state.notes && state.notes.map(item => (
           <div key={item.id} className="flex items-center">
             <li className="list pa1 f3">
               {item.note}
